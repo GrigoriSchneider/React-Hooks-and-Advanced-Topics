2fe38ca1436cdc204124e07e5aa93962d29fabde
GrigoriSchneider <grigorischneider@gmail.com>
2022-09-11 22:09:13 +0200

    added useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,38 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  // useEffect(() => {
+  //   console.log('call useEffect');
+  //   if (value >= 1) {
+  //     document.title = `New Messages (${value})`;
+  //   }
+  // });
+
+  // useEffect(() => {
+  //   console.log('call useEffect');
+  //   if (value >= 1) {
+  //     document.title = `New Messages (${value})`;
+  //   }
+  // }, []); // with the empty array as the second argument it only run ones with the initial render
+
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value >= 1) {
+      document.title = `New Messages (${value})`;
+    }
+  }, [value]); // with the value in the array as the second argument it only runs when value is changed
+
+  console.log('render component');
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,36 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
