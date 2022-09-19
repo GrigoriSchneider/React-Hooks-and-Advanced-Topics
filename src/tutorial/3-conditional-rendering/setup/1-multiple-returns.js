59b2691926909d4162b078b72d84d9f8ea5c2d6b
GrigoriSchneider <grigorischneider@gmail.com>
2022-09-19 21:18:13 +0200

    finished multiple returns and short circuit

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // if isloading true loading screen will show up
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('default user');
 
   useEffect(() => {
     fetch(url)
-      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true); // example shows up when url is wrong
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
       .catch((error) => console.log(error));
   }, []);
 
-  if (loading) {
+  // if loading is true then loading screen show up
+  if (isLoading) {
     return (
       <div>
         <h2>Loading...</h2>
@@ -25,12 +39,13 @@ const MultipleReturns = () => {
       </div>
     );
   }
-
   return (
     <div>
       <h1>{user}</h1>
     </div>
   );
+
+  return <h2>multiple returns</h2>;
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,26 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(true);
 
-  return <h2>short circuit</h2>;
+  // const firstValue = text || 'hello world'; // when text string is empty 'hello is '
+  // const secondValue = text && 'hello world'; // will only show when text is not an empty string
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {/* {text && <h1>hello world</h1>} */}
+      {/* {!text && <h1>hello world</h1>} */}
+      {isError && <h2>Error... biatch</h2>}
+      {isError ? <p>there kaka error</p> : <p>there is no kaka error</p>}
+    </>
+  );
 };
 
 export default ShortCircuit;
